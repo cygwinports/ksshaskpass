ab42cf7d18f9521a964fcaa1ee4232aa281b1291
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2011-05-20 03:30:05 -0500

    ksshaskpass 0.5.3

---
+++ postinstall.sh
@@ -0,0 +1,1 @@
+/usr/sbin/update-alternatives --install "/usr/sbin/ssh-askpass" "ssh-askpass" "/usr/lib/kde4/libexec/ksshaskpass.exe" 200
---
+++ preremove.sh
@@ -0,0 +1,1 @@
+/usr/sbin/update-alternatives --remove "ssh-askpass" "/usr/lib/kde4/libexec/ksshaskpass.exe"
