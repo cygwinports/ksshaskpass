ba3787b247c4e80c6d1b251a01d7ec50070f934f
Yaakov Selkowitz <yselkowi@redhat.com>
2017-02-23 01:41:08 -0600

    ksshaskpass 5.8.5

--- postinstall.sh
+++
@@ -1,1 +0,0 @@
-/usr/sbin/update-alternatives --install "/usr/sbin/ssh-askpass" "ssh-askpass" "/usr/lib/kde4/libexec/ksshaskpass.exe" 200
--- preremove.sh
+++
@@ -1,1 +0,0 @@
-/usr/sbin/update-alternatives --remove "ssh-askpass" "/usr/lib/kde4/libexec/ksshaskpass.exe"
